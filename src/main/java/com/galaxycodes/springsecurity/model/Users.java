ffc96f88af03723f44c1b635cf33f8064723c9ec
Thomas <thomasmumo101@gmail.com>
2025-02-13 18:51:15 -0800

    tiba

--- src/main/java/com/galaxycodes/springsecurity/model/Users.java
+++ src/main/java/com/galaxycodes/springsecurity/model/Users.java
@@ -25,9 +25,6 @@ public class Users {
     private Integer phoneNumber;
 
     @Lob
-    @JsonIgnore  // Prevents JSON serialization errors
-    @Basic(fetch = FetchType.LAZY)  // Prevents errors due to lazy loading
-    @Column(columnDefinition = "BYTEA")  // PostgreSQL LOB type
     private byte[] profileImageData;
     private String email;
 
