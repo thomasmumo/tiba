d9b51f4a64d6dba0c5d6f66a00f0da992c89d063
Thomas <thomasmumo101@gmail.com>
2025-03-01 12:47:11 -0800

    tiba

--- src/main/java/com/galaxycodes/springsecurity/model/Hospitals.java
+++ src/main/java/com/galaxycodes/springsecurity/model/Hospitals.java
@@ -1,6 +1,7 @@
 package com.galaxycodes.springsecurity.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
@@ -39,6 +40,7 @@ public class Hospitals {
 
     private List<Appointments> appointments;
     @ManyToMany(mappedBy = "hospitals")
+    @JsonIgnore
     private List<Patients> patients;
 
     public Hospitals() {
