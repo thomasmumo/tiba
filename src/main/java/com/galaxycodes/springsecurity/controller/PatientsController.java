73e18560c60402e95ca78008fb6184d7468dc267
Thomas <thomasmumo101@gmail.com>
2025-02-28 18:44:43 -0800

    tiba

--- src/main/java/com/galaxycodes/springsecurity/controller/PatientsController.java
+++ src/main/java/com/galaxycodes/springsecurity/controller/PatientsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashMap;
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class PatientsController {
     @Autowired
