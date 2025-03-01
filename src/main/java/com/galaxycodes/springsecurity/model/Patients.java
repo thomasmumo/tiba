f96d0e163fa993c6ec85d8a17ae0c699c7019689
Thomas <thomasmumo101@gmail.com>
2025-03-01 13:54:10 -0800

    tiba

--- src/main/java/com/galaxycodes/springsecurity/model/Patients.java
+++ src/main/java/com/galaxycodes/springsecurity/model/Patients.java
@@ -1,6 +1,7 @@
 package com.galaxycodes.springsecurity.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
@@ -56,6 +57,7 @@ public class Patients {
                     @JoinColumn(name = "hospital_id")
             }
     )
+    @JsonIgnore
     private List<Hospitals> hospitals;
 
     public Patients() {
